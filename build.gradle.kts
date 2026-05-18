2ebe51d977ee2b7ee8812c4130c48e4058639cfe
OpenARDF <66529806+OpenARDF@users.noreply.github.com>
2026-05-18 11:38:01 -0400

    Release SerialSlinger 2.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
 }
 
-val serialSlingerVersion = "2.0.3"
+val serialSlingerVersion = "2.0.4"
 // Bump this suffix for every new testable change set so desktop and Android builds expose
 // an unmistakable version string during iterative testing. Clear it before a full release.
 val serialSlingerVersionSuffix = ""
